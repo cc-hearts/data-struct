bddf7ff6f9804372e5063dd9d74a66c9d2ba6004
heart <7362469@qq.com>
2022-10-15 00:05:56 +0800

    docs: kmp add comments

--- ts/core/kmp.ts
+++ ts/core/kmp.ts
@@ -2,13 +2,16 @@
  * @author heart
  * @description KMP算法
  * @Date 2022-10-14
+ * @see: https://www.ruanyifeng.com/blog/2013/05/Knuth%E2%80%93Morris%E2%80%93Pratt_algorithm.html
+ * @see: https://www.zhihu.com/question/21923021
  */
 
 // 计算模式匹配移动的次数
 function calculateMaxMatchLength(pattern: string) {
   // 保存每个下标为止的最大的前后缀匹配
   const arr: number[] = Array.from({ length: pattern.length })
-  let maxLength = 0 // 保存模式匹配的最长的数字
+  let maxLength = 0 // 保存模式匹配的最大的个数
+  // for 相当于后缀 maxLength 相当于前缀
   for (let i = 1; i < pattern.length; i++) {
     //最大匹配不匹配的时候 匹配次大匹配
     while (maxLength > 0 && pattern.charAt(maxLength) !== pattern.charAt(i)) {
@@ -30,7 +33,7 @@ function kmpSearch(text: string, pattern: string) {
   // 存储所有的匹配的位置的开头
   const result: number[] = []
   const maxLengths = calculateMaxMatchLength(pattern)
-  let count = 0 // 位置偏移的值
+  let count = 0 // pattern位置偏移的值
   for (let i = 0; i < text.length; i++) {
     while (count > 0 && text.charAt(i) !== pattern.charAt(count)) {
       // 找到当前最大匹配的次大匹配 然后再开始比较
